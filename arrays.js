aa90d225abf561da782e5467d3a735887b788f1a
Jeremy Parker <jrobinparker@gmail.com>
2018-07-08 04:02:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,3 +42,8 @@ function removeElementFromBeginningOfArray(array) {
   array = [1, 2, 3, 4, 5]
   array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array = [1, 2, 3, 4, 5]
+  array.pop()
+}
